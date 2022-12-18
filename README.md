dbe1ff92a55c6965c314eef3ad7922b5fbdc72e4
Norbert Herczeg <noherczeg@gmail.com>
2022-12-18 22:50:18 +0100

    update repo

---
+++ README.md
@@ -0,0 +1,21 @@
+# system-react
+
+React 18 + Rollup + SystemJS playground
+
+## Setup
+
+```bash
+pnpm i
+```
+
+## Build
+
+```bash
+pnpm build
+```
+
+## Test
+
+```bash
+pnpm preview
+```
--- rollup.config.js
+++ rollup.config.js
@@ -1,26 +1,64 @@
+import { readFileSync } from 'node:fs';
+import { normalize } from 'node:path';
 import typescript from '@rollup/plugin-typescript';
 import terser from '@rollup/plugin-terser';
-import replace from '@rollup/plugin-replace';
 import commonjs from '@rollup/plugin-commonjs';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
+import clear from 'rollup-plugin-clear';
+import copy from 'rollup-plugin-copy';
+import define from 'rollup-plugin-define';
 
-import pkg from './package.json';
+const pkg = JSON.parse(readFileSync(normalize('./package.json'), { encoding:'utf8', flag:'r' }).toString());
+
+const ENV = process.env.NODE_ENV;
+
+function parseDependencyJSON(depName) {
+    const from = normalize(`./node_modules/${depName}/package.json`);
+    return JSON.parse(readFileSync(from, { encoding:'utf8', flag:'r' }).toString());
+}
+
+function getEffectiveVersionForDependency(d) {
+    return parseDependencyJSON(d).version;
+}
+
+function systemCopyObject(depName, suffix, skipFolder = false) {
+    return {
+        src: `node_modules/${depName}` + (suffix ? suffix : '') + (!skipFolder ? '/*' : ''),
+        dest: `dist/${depName}@${getEffectiveVersionForDependency(depName)}` + (!skipFolder ? suffix : ''),
+    };
+}
 
 export default {
     input: 'src/index.tsx',
     output: {
         dir: 'dist',
-        format: 'system'
+        format: 'system',
+        sourcemap: ENV === 'production',
     },
     plugins: [
+        clear({
+            targets: ['dist'],
+        }),
+        define({
+            replacements: {
+                'process.env.NODE_ENV': '"production"', // shake off jsx-runtime dev-mode...
+            }
+        }),
         commonjs(),
         nodeResolve(),
-        replace({
-            'process.env.NODE_ENV': JSON.stringify('production'),
-            __buildDate__: () => JSON.stringify(new Date()),
-            __buildVersion: pkg.dependencies.react.replace(/[\^~]/g, ''),
-        }),
         typescript(),
-        terser(),
+        // ...[ENV === 'production' ? terser() : undefined],
+        copy({
+            targets: [
+                { src: 'public/*', dest: 'dist' },
+                systemCopyObject('systemjs', '/dist'),
+                systemCopyObject('react', '/umd'),
+                systemCopyObject('react-dom', '/umd'),
+                systemCopyObject('@mui/material', '/umd'),
+            ],
+        }),
+    ],
+    external: [
+        ...Object.keys(pkg.dependencies)
     ],
 };
